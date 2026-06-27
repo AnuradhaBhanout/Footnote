80cd9a8b52364691a71972b0f68e4e12e83d92bb
AnuradhaBhanout <anuradha0042@gmail.com>
2026-06-28 02:51:33 +0530

    working on semantic_cache.py

---
+++ src/Semantic_cache.py
@@ -0,0 +1,27 @@
+import os
+import pickle
+import hashlib
+import time
+import numpy as np
+
+
+SIMILARITY_THRESHOLD = 0.92
+
+class SemanticCache:
+    def __init__(self,model,cache_file: str = os.path.join("papers","_semantic_cache.pk1")):
+        self.model = model
+        self.cache_file = cache_file
+        self.entries: list = []  # this will hold cached answers in memory
+        self._load()
+
+
+    def _load(self):
+        if os.path.isfile(self.cache_file):
+            with open(self.cache_file,"rb") as f:
+               # 'pickle' converts the python list into a file
+               self.entries = pickle.load(f)
+    
+    def _save(self):
+        with open(self.cache_file,"wb") as f:
+            pickle.dump(self.entries,f)
+        
