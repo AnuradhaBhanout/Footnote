536444c96d63d12cdc541b56799cf98604bb5bdf
AnuradhaBhanout <anuradha0042@gmail.com>
2026-07-01 22:42:21 +0530

    adding get_conn()  function to open a "pipeline" (connection) to Postgres

--- src/db.py
+++ src/db.py
@@ -1,7 +1,15 @@
 
 import os
+import psycopg2
+from pgvector.psycopg2 import register_vector
 from dotenv import load_dotenv,find_dotenv
 
 
 _ = load_dotenv(find_dotenv())
 DATABASE_URL = os.getenv("DATABASE_URL")
+
+def get_conn():
+    """Return a psycopg2 connection with pgvectortype registered."""
+    conn = psycopg2.connect(DATABASE_URL)
+    register_vector(conn)
+    return conn
