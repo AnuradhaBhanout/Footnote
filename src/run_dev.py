f580e8f7ad72029e86ec492af06a7d86f3e8e067
AnuradhaBhanout <anuradha0042@gmail.com>
2026-09-03 00:52:20 +0530

    updated run

--- src/run_dev.py
+++ src/run_dev.py
@@ -1,6 +1,11 @@
-import sys, asyncio
-if sys.platform == "win32":
-    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-
+import sys
+import asyncio
 import uvicorn
-uvicorn.run("api.api:app", host="127.0.0.1", port=8000, workers=1)
+
+config = uvicorn.Config("api.api:app", host="127.0.0.1", port=8000)
+server = uvicorn.Server(config)
+
+if sys.platform == "win32":
+    asyncio.run(server.serve(), loop_factory=asyncio.SelectorEventLoop)
+else:
+    server.run()
