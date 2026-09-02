e60bcf7b6e5ceb132d12a78a151ab78e199cf0e4
AnuradhaBhanout <anuradha0042@gmail.com>
2026-09-03 01:25:11 +0530

    removed lifespan

--- src/server/research_server.py
+++ src/server/research_server.py
@@ -25,39 +25,4 @@ logging.basicConfig(
 
 
 
-from server .mcp_app import mcp
-from server import tools
-
-
-if __name__ == "__main__":
-    import asyncio
-    import uvicorn
-    from arq.worker import Worker
-    from server.worker import WorkerSettings
-    from contextlib import asynccontextmanager
-    from starlette.applications import Starlette
-    from starlette.routing import Mount
-
-    mcp_app = mcp.sse_app()
-
-    @asynccontextmanager
-    async def lifespan(app):
-        worker = Worker(
-            functions=WorkerSettings.functions,
-            redis_settings = WorkerSettings.redis_settings,
-            handle_signals=False,
-        )       
-
-        worker_task = asyncio.create_task(worker.async_run())
-
-        try:
-            yield
-        finally:
-            worker_task.cancel()
-            await worker.close()
-
-    app = Starlette(routes=[Mount("/", app=mcp_app)], lifespan=lifespan)
-
-    uvicorn.run(app, host="0.0.0.0",port=int(os.environ.get("PORT") or 8001))
-
 
