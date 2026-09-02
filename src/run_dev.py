2b66612ad113cb3a21d67ab0d85394318424c514
AnuradhaBhanout <anuradha0042@gmail.com>
2026-09-02 23:41:05 +0530

    run command

---
+++ src/run_dev.py
@@ -0,0 +1,6 @@
+import sys, asyncio
+if sys.platform == "win32":
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+import uvicorn
+uvicorn.run("api.api:app", host="127.0.0.1", port=8000, workers=1)
